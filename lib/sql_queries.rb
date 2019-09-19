6f90b5e6497a1d9a5a085ec9a71c52da8902082f
Randy Taylor <randyisbusy@gmail.com>
2019-09-19 21:55:53 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -19,7 +19,7 @@ def select_youngest_bear_and_returns_name_and_age
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "SELECT COUNT(color) AS abc FROM bears ORDER BY abx DESC LIMIT 1;"
+  "SELECT COUNT(color) AS abc FROM bears ORDER BY abc DESC LIMIT 1;"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
