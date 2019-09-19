035eb8d75423cd8c1ba642f8fb94f7d636e4dd5b
Randy Taylor <randyisbusy@gmail.com>
2019-09-19 22:28:34 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -24,8 +24,7 @@ def selects_most_prominent_color_and_returns_with_count
 end
 
 def counts_number_of_bears_with_goofy_temperaments
-  "SELECT temperament AS perfect FROM bears WHEN temperament = 'goofy'
-  COUNT(perfect);"
+  "SELECT temperament, COUNT(temperament) FROM bears WHEN temperament = 'goofy';"
 end
 
 def selects_bear_that_killed_Tim
